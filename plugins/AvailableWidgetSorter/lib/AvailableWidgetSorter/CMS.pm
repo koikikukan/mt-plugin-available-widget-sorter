f8c6555b428365204430b18d6265d76e3e14b0ee
koikikukan <youjiroh@wd5.so-net.ne.jp>
2011-01-04 18:31:42 +0900

    add sort_order select

--- plugins/AvailableWidgetSorter/lib/AvailableWidgetSorter/CMS.pm
+++ plugins/AvailableWidgetSorter/lib/AvailableWidgetSorter/CMS.pm
@@ -4,9 +4,17 @@ use strict;
 
 sub edit_template_param {
     my ($cb, $app, $param, $tmpl) = @_;
+
+    my $plugin = MT->component("AvailableWidgetSorter");
+    my $sort_order = $plugin->get_config_value('sort_order', 'blog:'.$app->blog->id);
     my $avail_modules = $param->{available};
 
-    my @sorted_avail_modules = sort { $a->{name} cmp $b->{name} } @$avail_modules; 
+    my @sorted_avail_modules;
+    if ($sort_order eq 'descend') {
+        @sorted_avail_modules = sort { $b->{name} cmp $a->{name} } @$avail_modules;
+    } else {
+        @sorted_avail_modules = sort { $a->{name} cmp $b->{name} } @$avail_modules;
+    }
     $param->{available} = \@sorted_avail_modules;
 }
 
